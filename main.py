d0e996baff1f148d62d69f33675708957ef372b7
DPatrick76 <dougpat76@gmail.com>
2016-09-01 19:29:07 -0500

    Files to Demo to TA

--- main.py
+++ main.py
@@ -1,24 +1,59 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 import webapp2
+from caesar import encrypt
+import cgi
 
+form="""
+        <html>
+    <head>
+        <style>
+            form {
+                background-color:#ccff99;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }
+            textarea {
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }
+            p.error {
+                color: red;
+            }
+        </style>
+    </head>
+    <body>
+        <form method="post">
+            <div>
+                <label for="rot">Rotate by:</label>
+                <input type="text" name="rot" value="0">
+                <p class="error"></p>
+            </div>
+            <textarea type="text" name="text">%(value)s</textarea>
+            <br>
+            <input type="submit">
+        </form>
+    </body>
+</html>
+"""
 class MainHandler(webapp2.RequestHandler):
+    def write_form(self, value=""):
+        self.response.out.write(form % {"value":value})
+
     def get(self):
-        self.response.write('Hello world!')
+        self.write_form()
+
+    def post(self):
+        userNumber = self.request.get('rot')
+        userNumber = int(userNumber)
+        userText = self.request.get("text")
+        userText = cgi.escape(userText, quote=True)
+        userText = encrypt(userText,userNumber)
+        #userText = cgi.escape(userText, quote=True)
+        self.write_form(userText)
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
